af0fb61cf5343b96c198ec304d4db22f4fd3acb6
Joel Wolf <72318130+joel-wlf@users.noreply.github.com>
2023-12-19 20:33:45 +0000

    add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import './pages/Dashboard'
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
