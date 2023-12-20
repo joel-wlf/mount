2633f7bc97dedd417e6ab165555bec29f11ef626
Joel <72318130+joel-wlf@users.noreply.github.com>
2023-12-20 16:48:34 +0100

    added geist ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { CssBaseline, GeistProvider } from '@geist-ui/core'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GeistProvider themeType="dark">
+      <CssBaseline />
+      <App />
+    </GeistProvider>        
   </React.StrictMode>,
 )
