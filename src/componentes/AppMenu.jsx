c41617dfbbd5b0958ef687cac92c6d069d527158
Joel <72318130+joel-wlf@users.noreply.github.com>
2023-12-22 15:16:31 +0100

    added icons to Menu Items

--- src/componentes/AppMenu.jsx
+++ src/componentes/AppMenu.jsx
@@ -1,5 +1,5 @@
 import { User, Button, Grid } from '@geist-ui/core'
-import { X, LogOut } from '@geist-ui/icons'
+import { X, LogOut, Layout } from '@geist-ui/icons'
 import { useBodyScroll } from '@geist-ui/core'
 import { useEffect } from 'react'
 import MenuItem from './MenuItem'
@@ -21,16 +21,16 @@ function AppMenu(props) {
         <>
             <nav>
                 <User name="Joel Wolf" src='./profile-pic.jpeg' className='fadein' style={{animationDelay: '50ms'}} marginRight="auto" />
-                <Button auto type="abort" icon={<LogOut />} className='fadein' onClick={logOut} padding="5px" height="45px" margin="none"></Button> 
-                <Button auto type="abort" icon={<X />} className='fadein' onClick={props.handleClick} padding="5px" height="50px" margin="none"></Button> 
+                <Button auto type="abort" icon={<LogOut />} className='fadein' onClick={logOut} padding="5px" height="48px" margin="none"></Button> 
+                <Button auto type="abort" icon={<X />} className='fadein' onClick={props.handleClick} padding="5px" height="53px" margin="none"></Button> 
             </nav>
             <main className="menu">
             <Grid.Container gap={0}>
-                <MenuItem icon="Arrow" text="Test" page="/test" child="1" />
-                <MenuItem icon="Arrow" text="Test" page="/test" child="2" />
-                <MenuItem icon="Arrow" text="Test" page="/test" child="3" />
-                <MenuItem icon="Arrow" text="Test" page="/test" child="4" />
-                <MenuItem icon="Arrow" text="Test" page="/test" child="5" />
+                <MenuItem icon={<Layout />} text="Test" page="/test" child="1" />
+                <MenuItem icon={<X />} text="Test" page="/test" child="2" />
+                <MenuItem icon={<X />} text="Test" page="/test" child="3" />
+                <MenuItem icon={<X />} text="Test" page="/test" child="4" />
+                <MenuItem icon={<X />} text="Test" page="/test" child="5" />
             </Grid.Container>
             </main>
         </>
--- src/componentes/MenuItem.jsx
+++ src/componentes/MenuItem.jsx
@@ -1,12 +1,19 @@
 import { Grid, Button, Divider } from '@geist-ui/core'
+import {ArrowRight} from '@geist-ui/icons'
 
 
 function MenuItem(props) {
+
+    
+
     return (
         <>
             {props.child != 1 && <Divider className='fadein' style={{width:'100%', animationDelay: `${props.child * 50}ms`}} />}
-            <Grid xs={24} direction='column'>
-                <p className='fadein menu--item' style={{animationDelay: `${props.child * 50}ms`}}>{props.text}</p>        
+            <Grid xs={24} direction='row' alignItems='center'>
+                <div className="menu--item-cell fadein" style={{animationDelay: `${props.child * 50}ms`}}>
+                    {props.icon}
+                    <p className='menu--item'>{props.text}</p>        
+                </div>
             </Grid>
         </>
 
