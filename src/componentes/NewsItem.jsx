d51713c8807e7d4bf100ef9c18e53fac0b17d2ef
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-08 16:06:15 +0100

    NewsItem grid

--- src/componentes/NewsItem.jsx
+++ src/componentes/NewsItem.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 function NewsItem(props) {
+    
     return (
-        <>
-            
-        </>
+        <div className="news-article">
+            <div className="img-wrapper" style={{backgroundImage: "url(profile-pic.jpeg)", backgroundRepeat: "no-repeat", backgroundSize: "150%", backgroundPosition: "center"}}>
+            </div>
+        </div>
     )
 }
 
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,4 +1,5 @@
 import Navbar from '../componentes/Navbar'
+import NewsItem from '../componentes/NewsItem'
 import { useBodyScroll, Button, Spacer } from '@geist-ui/core'
 import { SlidersHorizontal, Plus } from 'lucide-react'
 
@@ -16,8 +17,11 @@ function News() {
             <div className='content'>
                 <h2>News</h2>
                 <div className="news--actions">
-                    <Button width="80%" type="secondary" icon={<Plus />}>Post News</Button>
+                    <Button width="78%" type="secondary" icon={<Plus />}>Post News</Button>
                     <Button width="20%" icon={<SlidersHorizontal />}></Button>
+                </div>
+                <div className="news--articles">
+                    <NewsItem />
                 </div>
             </div>
         </>
