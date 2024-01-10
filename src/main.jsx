81ff844816a53ca62fed4aa6020d8548e0c52ae4
Joel Wolf <72318130+joel-wlf@users.noreply.github.com>
2024-01-10 13:03:38 +0000

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { CssBaseline, GeistProvider } from '@geist-ui/core'
+import { Analytics } from '@vercel/analytics/react';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <GeistProvider themeType="dark">
       <CssBaseline />
       <App />
-    </GeistProvider>        
+    </GeistProvider>       
+    <Analytics /> 
   </React.StrictMode>,
 )
