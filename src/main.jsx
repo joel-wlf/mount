a38f9212fef35c176e425ab4ed03304873f0da7f
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-12 17:38:37 +0100

    removed vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { CssBaseline, GeistProvider } from '@geist-ui/core'
-import { Analytics } from '@vercel/analytics/react';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <GeistProvider themeType="dark">
       <CssBaseline />
       <App />
-    </GeistProvider>       
-    <Analytics /> 
-  </React.StrictMode>,
+    </GeistProvider>
+  </React.StrictMode>
 )
