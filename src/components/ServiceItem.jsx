931d97e4694744409b1ff9d641a8f88f7bab36c1
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-18 20:56:38 +0100

    fixed spelling mistake

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -19,7 +19,7 @@ function EnergyStorage(props) {
         <div className="flex items-center justify-center w-full p-3">
             <div className="group flex items-center justify-center flex-col w-[90%] p-5 border-[1px] border-[#333] hover:border-[#555] duration-300 rounded-[10px]">
               <Icon />
-              <h3 className="group-hover:-transla te-y-1 group-hover:scale-110 transition-all">{props.title}</h3>
+              <h3 className="group-hover:-translate-y-1 group-hover:scale-110 transition-all">{props.title}</h3>
               <Button>{props.buttonText}</Button>
             </div>
         </div>
