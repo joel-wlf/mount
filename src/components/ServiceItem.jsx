68c505464463f5baf2aea894ac1bfb7e426121ee
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-18 20:51:40 +0100

    added props to ServiceItem

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -1,13 +1,26 @@
-import { Package } from "lucide-react";
 import { Button } from "@geist-ui/core";
+import { CarTaxiFront, Package, CalendarClock, Utensils } from "lucide-react";
 
 function EnergyStorage(props) {
+
+  function Icon() {
+    if (props.icon == "CarTaxiFront") {
+      return <CarTaxiFront className="group-hover:-translate-y-1 group-hover:scale-75 transition-all" size={52}/>
+    } else if (props.icon == "Package") {
+      return <Package className="group-hover:-translate-y-1 group-hover:scale-75 transition-all" size={52}/>
+    } else if (props.icon == "CalendarClock") {
+      return <CalendarClock className="group-hover:-translate-y-1 group-hover:scale-75 transition-all" size={52}/>
+    } else if (props.icon == "Utensils") {
+      return <Utensils className="group-hover:-translate-y-1 group-hover:scale-75 transition-all" size={52}/>
+    }
+  }
+  
   return (
         <div className="flex items-center justify-center w-full p-3">
             <div className="group flex items-center justify-center flex-col w-[90%] p-5 border-[1px] border-[#333] hover:border-[#555] duration-300 rounded-[10px]">
-              <Package className="group-hover:-translate-y-1 group-hover:scale-75 transition-all" size={52}/>
-              <h3 className="group-hover:-translate-y-1 group-hover:scale-110 transition-all">Package Delivery</h3>
-              <Button>Send Package</Button>
+              <Icon />
+              <h3 className="group-hover:-transla te-y-1 group-hover:scale-110 transition-all">{props.title}</h3>
+              <Button>{props.buttonText}</Button>
             </div>
         </div>
   );
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,22 +1,36 @@
 import Navbar from '../components/Navbar'
 import ServiceItem from '../components/ServiceItem'
+import { useState } from 'react'
 import { useBodyScroll } from '@geist-ui/core'
 
 
 function Services() {
     
     const [hidden, setHidden] = useBodyScroll()
+
+    const [service, setService] = useState(<Menu />)
     
     function toggleScroll(state) {
         setHidden(state)
-    }      
-
+    }     
+    
+    function Menu() {
+        return (
+            <>
+                <h2>Services</h2>
+                <ServiceItem title="Package Pickup" icon="Package" buttonText="Send Package" />
+                <ServiceItem title="Package Pickup" icon="Package" buttonText="Send Package" />
+                <ServiceItem title="Package Pickup" icon="Package" buttonText="Send Package" />
+                <ServiceItem title="Package Pickup" icon="Package" buttonText="Send Package" />
+            </>
+        )
+    }
+    
     return (
         <>
             <Navbar toggleScroll={toggleScroll} />
             <div className='content'>
-                <h2>Services</h2>
-                <ServiceItem />
+                <Menu />
             </div>
         </>
     )
