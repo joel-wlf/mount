35462036f5e8b085c4cc26d98fbcf4e5fd97de83
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-18 21:16:59 +0100

    removed Tabs

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -19,17 +19,12 @@ function Services() {
         return (
             <>
                 <h2>Services</h2>
-                <Tabs initialValue="1" hideDivider align="center" leftSpace={0}>
-                    <Tabs.Item label={<>Spend</>} value="1">
-                        <Divider className='z-0'>Community</Divider>
-                        <ServiceItem title="Ride Share" icon="CarTaxiFront" buttonText="Discover Drivers" />
-                        <ServiceItem title="Package Pickup" icon="Package" buttonText="Send Package" />
-                        <Divider className='z-0'>Corporate</Divider>
-                        <ServiceItem title="Event Planning" icon="CalendarClock" buttonText="Book Event" />
-                        <ServiceItem title="Restaurant" icon="Utensils" buttonText="Reserve Table" />
-                    </Tabs.Item>
-                    <Tabs.Item label={<>Earn</>} value="2"></Tabs.Item>
-                </Tabs>
+                    <Divider className='z-0'>Community</Divider>
+                    <ServiceItem title="Ride Share" icon="CarTaxiFront" buttonText="Discover Drivers" />
+                    <ServiceItem title="Package Pickup" icon="Package" buttonText="Send Package" />
+                    <Divider className='z-0'>Corporate</Divider>
+                    <ServiceItem title="Event Planning" icon="CalendarClock" buttonText="Book Event" />
+                    <ServiceItem title="Restaurant" icon="Utensils" buttonText="Reserve Table" />
             </>
         )
     }
