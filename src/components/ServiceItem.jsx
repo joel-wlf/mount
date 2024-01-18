5722a3910fe792d409dbbe6045acdc60fa000df9
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-18 20:22:04 +0100

    update ServiceItem.jsx

--- src/components/ServiceItem.jsx
+++ src/components/ServiceItem.jsx
@@ -1,11 +1,14 @@
-import { Card } from "@geist-ui/core"
+import { Package } from "lucide-react";
+import { Button } from "@geist-ui/core";
 
 function EnergyStorage(props) {
   return (
         <div className="flex items-center justify-center w-full p-3">
-        <Card  width="90%" key={props.title}>
-            <h3>Package Delivery</h3>
-        </Card>
+            <div className="group flex items-center justify-center flex-col w-[90%] p-5 border-[1px] border-[#333] hover:border-[#555] duration-300 rounded-[10px]">
+              <Package className="group-hover:-translate-y-1 group-hover:scale-75 transition-all" size={52}/>
+              <h3 className="group-hover:-translate-y-1 group-hover:scale-110 transition-all">Package Delivery</h3>
+              <Button>Send Package</Button>
+            </div>
         </div>
   );
 }
