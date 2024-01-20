9457c3ed288f1b2c9ed84aef8576e9e86f656c34
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-20 11:24:50 +0100

    added article bodies

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -7,11 +7,13 @@ export default [
     {
         title: "Erntebericht 2028",
         description: "Der Erntebericht für 2028 ist veröffentlicht und kann aufgerufen werden ...",
-        image: "agrar.jpg"
+        image: "agrar.jpg",
+        body: "Im Agrarbericht 2028 für unser innovatives Dorf verzeichnen wir beeindruckende Fortschritte. Die Ernteerträge sind um 30% gestiegen, dank fortschrittlicher Anbautechnologien und nachhaltiger Bewirtschaftungsmethoden. Die Einführung von Smart-Farming-Systemen hat die Effizienz verbessert und den Ressourcenverbrauch reduziert. Zudem haben wir erfolgreich alternative Energiequellen in der Landwirtschaft integriert, was zu einer 40%igen Senkung der CO2-Emissionen geführt hat. Das innovative Dorf zeigt sich als Vorreiter in der nachhaltigen Landwirtschaft, wodurch sowohl ökologische als auch wirtschaftliche Ziele erreicht werden. Den Agrarbericht kann auf der Seite des Bundesagrarminesterium aufgerufen werden."
     },
     {
         title: "Treffen Gartenverein",
         description: "Der Erntebericht für 2028 ist veröffentlicht und kann aufgerufen ...",
-        image: "flower-shop.jpg"
+        image: "flower-shop.jpg",
+        body: "Liebe Dorfgemeinschaft, auch in diesem Jahr können wir Ihnen mit größter Freude mitteilen, dass unser Blumenladen über den ganzen Winter weiterhin geöffnet ist. Dies verdanken wir nur unserem Dorfrat, der uns ein paar der innovativen Gewächshäusern zu Verfügung gestellt hat. Somit können wir das ganze Jahr in diesen beheizten und Solarbetriebenen Gewächshäusern unsere Blumen anpflanzen und sie das ganze Jahr verkaufen."
     }
 ]
