1dfcf0efd615dad39c1176b5476621df4647764e
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-22 22:08:54 +0100

    added sort

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,5 +1,6 @@
 import { Button, useBodyScroll } from '@geist-ui/core';
-import { Plus, SlidersHorizontal } from 'lucide-react';
+import { ArrowDown01, ArrowUp10, Plus } from 'lucide-react';
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import NewsItem from '../components/NewsItem';
 import newsData from '../data/newsData';
@@ -7,17 +8,34 @@ import newsData from '../data/newsData';
 function News() {
   const [hidden, setHidden] = useBodyScroll();
 
+  const [order, setOrder] = useState('desc');
+
+  const [localArticles, setLocalArticles] = useState(() =>
+    JSON.parse(localStorage.getItem('localArticles'))
+  );
+
   function toggleScroll(state) {
     setHidden(state);
   }
 
-  const localArticles = JSON.parse(localStorage.getItem('localArticles'));
+  function toggleOrder() {
+    order == 'desc' ? setOrder('asc') : setOrder('desc');
+  }
 
   if (localArticles) {
     var articles = newsData.concat(localArticles);
   } else if (!localArticles) {
     var articles = newsData;
   }
+
+  const compareDates = (a, b) => {
+    const dateA = new Date(a.date.split('.').reverse().join('-'));
+    const dateB = new Date(b.date.split('.').reverse().join('-'));
+
+    return order == 'desc' ? dateB - dateA : dateA - dateB;
+  };
+
+  articles.sort(compareDates);
 
   const articleItems = articles.map((article) => (
     <NewsItem key={article.title} handleScroll={setHidden} data={article} />
@@ -32,7 +50,11 @@ function News() {
           <Button width="78%" type="secondary" icon={<Plus />}>
             Post News
           </Button>
-          <Button width="20%" icon={<SlidersHorizontal />}></Button>
+          <Button
+            width="20%"
+            onClick={toggleOrder}
+            icon={order == 'desc' ? <ArrowUp10 /> : <ArrowDown01 />}
+          ></Button>
         </div>
         <div className="news--articles">{articleItems}</div>
       </div>
