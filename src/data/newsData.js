a21c7bdc384bb258f119e60349c870d0e40ef33b
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-22 21:16:56 +0100

    update newsData.js

--- src/data/newsData.js
+++ src/data/newsData.js
@@ -5,6 +5,7 @@ export default [
     image: 'gardening.jpg',
     date: '12.05.2028',
     user: 'Gartenbau',
+    type: 'static',
     body: 'Nächsten Samstag lädt der Gartenbauverein zu einem gemeinsamen Treffen ein, wo wir uns über die Pflanzmethoden und die Neuerungen der Software unserer Gewächshäuser besprechen. Alle Dorfbewohner die ein Gewächshaus besitzen (also alle 😀) sind herzlich eingeladen zum Treffen zu kommen. Das Treffen startet nächsten Samstag gegen 10 Uhr. Alle sind herzlich eingeladen Snacks und Dips aus eigenem Anbau mitzubringen. Euer Gartenbauverein',
   },
   {
@@ -12,6 +13,9 @@ export default [
     description:
       'Der Erntebericht für 2028 ist veröffentlicht und kann aufgerufen werden ...',
     image: 'agrar.jpg',
+    date: '05.12.2028',
+    user: 'Bundesagrarminesterium',
+    type: 'static',
     body: 'Im Agrarbericht 2028 für unser innovatives Dorf verzeichnen wir beeindruckende Fortschritte. Die Ernteerträge sind um 30% gestiegen, dank fortschrittlicher Anbautechnologien und nachhaltiger Bewirtschaftungsmethoden. Die Einführung von Smart-Farming-Systemen hat die Effizienz verbessert und den Ressourcenverbrauch reduziert. Zudem haben wir erfolgreich alternative Energiequellen in der Landwirtschaft integriert, was zu einer 40%igen Senkung der CO2-Emissionen geführt hat. Das innovative Dorf zeigt sich als Vorreiter in der nachhaltigen Landwirtschaft, wodurch sowohl ökologische als auch wirtschaftliche Ziele erreicht werden. Den Agrarbericht kann auf der Seite des Bundesagrarminesterium aufgerufen werden.',
   },
   {
@@ -19,6 +23,9 @@ export default [
     description:
       'Der Erntebericht für 2028 ist veröffentlicht und kann aufgerufen ...',
     image: 'flower-shop.jpg',
+    date: '01.12.2028',
+    user: 'Hökers Blumenladen',
+    type: 'static',
     body: 'Liebe Dorfgemeinschaft, auch in diesem Jahr können wir Ihnen mit größter Freude mitteilen, dass unser Blumenladen über den ganzen Winter weiterhin geöffnet ist. Dies verdanken wir nur unserem Dorfrat, der uns ein paar der innovativen Gewächshäusern zu Verfügung gestellt hat. Somit können wir das ganze Jahr in diesen beheizten und Solarbetriebenen Gewächshäusern unsere Blumen anpflanzen und sie das ganze Jahr verkaufen.',
   },
 ];
