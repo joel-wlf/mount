b0062a5c95d1df6679cea998795a0b22054a9b0a
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-23 19:05:16 +0100

    update PostModal.jsx

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import { X } from 'lucide-react';
+import { Input } from '@geist-ui/core';
 import Backdrop from './Backdrop';
 
 function NewsModal({ handleClose, data }) {
@@ -25,13 +26,21 @@ function NewsModal({ handleClose, data }) {
   return (
     <Backdrop onClick={handleClose}>
       <motion.div
-        className="fixed overflow-y-scroll w-[80%] h-[90%] bg-black border-[1px] border-[#333] rounded-[10px]"
+        className="fixed overflow-y-scroll w-[80%] h-[90%] p-6 bg-black border-[1px] border-[#333] rounded-[10px]"
         onClick={(e) => e.stopPropagation()}
         variants={slideIn}
         initial="hidden"
         animate="visible"
         exit="exit"
-      ></motion.div>
+      >
+        <button
+          className="absolute top-0 right-0 p-1 m-3 bg-[rgba(0,0,0,0.4)] aspect-square rounded-full backdrop-blur-lg"
+          onClick={handleClose}
+        ></button>
+        <input type="file" />
+        <h3>Post News</h3>
+        <Input clearable placeholder="Title" name="title" />
+      </motion.div>
     </Backdrop>
   );
 }
