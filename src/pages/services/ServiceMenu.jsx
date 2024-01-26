399cf2b984c0a5007220ed3fe5a7dd96b3fb41c7
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-26 18:26:24 +0100

    added euro coins

--- src/pages/services/ServiceMenu.jsx
+++ src/pages/services/ServiceMenu.jsx
@@ -1,9 +1,21 @@
-import { Divider, useBodyScroll } from '@geist-ui/core';
+import { Divider, Button, useBodyScroll } from '@geist-ui/core';
+import { BadgeEuro, Coins } from 'lucide-react';
 import Navbar from '../../components/Navbar';
 import ServiceItem from '../../components/ServiceItem';
+import { useEffect, useState } from 'react';
 
 function ServiceMenu() {
   const [hidden, setHidden] = useBodyScroll();
+
+  const [euroCoins, setEuroCoins] = useState(
+    () => localStorage.getItem('euroCoins') || 120
+  );
+
+  useEffect(() => {
+    if (!localStorage.getItem('euroCoins')) {
+      localStorage.setItem('euroCoins', 120);
+    }
+  });
 
   function toggleScroll(state) {
     setHidden(state);
@@ -14,10 +26,18 @@ function ServiceMenu() {
       <Navbar toggleScroll={toggleScroll} />
       <div className="content">
         <h2>Services</h2>
+        <div className="flex flex-row items-center justify-start gap-3 w-full p-5 mb-5 border-[1px] border-[#333] hover:border-[#555] duration-300 rounded-[10px]">
+          <BadgeEuro size={45} />
+          <h2 className="m-0">{euroCoins}</h2>
+          <Button auto icon={<Coins />} scale={4 / 5} marginLeft="auto">
+            Send Coins
+          </Button>
+        </div>
         <Divider className="z-0">Community</Divider>
         <ServiceItem
           title="Ride Share"
           icon="CarTaxiFront"
+          routes={['/services/share-ride']}
           buttonText={['Discover drivers', 'Share your Ride']}
         />
         <ServiceItem
