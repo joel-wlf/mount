54a1216a903f2bf15fcb910f57a12585bbf5e8b2
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-29 15:46:01 +0100

    feat: create PersonCard.jsx

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,46 @@
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import NewsModal from './NewsModal';
+
+function NewsItem(props) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+    props.handleScroll(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+    props.handleScroll(false);
+  };
+
+  return (
+    <div
+      className="news-article"
+      onClick={() => (modalOpen ? closeModal() : openModal())}
+    >
+      <AnimatePresence initial={false} mode="wait">
+        {modalOpen && (
+          <NewsModal
+            data={props.data}
+            modalOpen={modalOpen}
+            handleClose={closeModal}
+          />
+        )}
+      </AnimatePresence>
+      <div className="article-image">
+        {props.data.type == 'static' ? (
+          <img src={`news/${props.data.image}`} />
+        ) : (
+          <img src={props.data.image} />
+        )}
+      </div>
+      <div className="article-info">
+        <h4>{props.data.title}</h4>
+        <p>{props.data.description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default NewsItem;
