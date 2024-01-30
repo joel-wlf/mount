b405bae028c07795c2d555c7a473df8fb7efdea9
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-01-30 20:18:45 +0100

    driver interface

--- src/pages/services/ShareYourRide.jsx
+++ src/pages/services/ShareYourRide.jsx
@@ -1,16 +1,17 @@
 import { Button, Input, useBodyScroll } from '@geist-ui/core';
 import gpc from 'generate-pincode';
-import { BadgeCheck, MapPinned } from 'lucide-react';
+import { BadgeCheck, MapPinned, UserSearch } from 'lucide-react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
-import PersonCard from '../../components/PersonCard';
 
 function ShareYourRide() {
   const [hidden, setHidden] = useBodyScroll();
 
   const [formData, setFormData] = useState({
     destination: '',
+    departure: '',
+    price: 0,
   });
 
   const navigate = useNavigate();
@@ -27,10 +28,17 @@ function ShareYourRide() {
     });
   }
 
-  function searchDrivers() {
-    if (formData.destination) {
+  function searchClients() {
+    if (formData.destination && formData.departure && formData.price) {
       setStep(2);
     }
+  }
+
+  function handleSubmit() {
+    let currentAmount = localStorage.getItem('euroCoins');
+    let newAmount = currentAmount + formData.price;
+    localStorage.setItem('euroCoins', newAmount);
+    navigate('/services');
   }
 
   return (
@@ -41,71 +49,64 @@ function ShareYourRide() {
         <div
           className={`${
             step != 1 && 'hidden'
-          } flex items-center justify-center gap-5 flex-col w-full h-[60vh] px-10`}
+          } flex items-center justify-center gap-5 flex-col w-full h-[70vh] px-10`}
         >
           <MapPinned size={120} />
           <Input
             width="100%"
             name="destination"
             onChange={handleChange}
-            placeholder="Pick destination..."
+            placeholder="Where are you going?"
+          />
+          <Input
+            width="100%"
+            name="departure"
+            onChange={handleChange}
+            placeholder="Approximate departure time "
+          />
+          <Input
+            width="100%"
+            name="price"
+            htmlType="number"
+            onChange={handleChange}
+            placeholder="Your Price"
           />
           <Button
             type="secondary"
-            disabled={formData.destination == ''}
-            onClick={searchDrivers}
+            disabled={
+              !formData.destination || !formData.departure || !formData.price
+            }
+            onClick={searchClients}
           >
-            Search drivers
+            Search Clients
           </Button>
         </div>
-        <div className={`${step != 2 && 'hidden'}`}>
-          <p className="text-gray-500 mt-0">
-            Available Drivers going in direction of "{formData.destination}":
+        <div
+          className={`${
+            step != 2 && 'hidden'
+          } flex items-center justify-center flex-col gap-5 h-[70vh]`}
+        >
+          <UserSearch size={120} />
+          <p className="text-gray-300 text-lg text-center m-0">
+            Anton requested a ride in direction of "{formData.destination}"
           </p>
-          <PersonCard
-            next={() => setStep(3)}
-            name="Anton"
-            byYou="16:32"
-            arrival="16:52"
-            price="14"
-          />
-          <PersonCard
-            next={() => setStep(3)}
-            name="Anton"
-            byYou="16:32"
-            arrival="16:52"
-            price="14"
-          />
-          <PersonCard
-            next={() => setStep(3)}
-            name="Anton"
-            byYou="16:32"
-            arrival="16:52"
-            price="14"
-          />
-          <PersonCard
-            next={() => setStep(3)}
-            name="Anton"
-            byYou="16:32"
-            arrival="16:52"
-            price="14"
-          />
+          <div className="flex items-center justify-center gap-5 flex-row w-full">
+            <Button ghost width="40%" type="error">
+              Cancel
+            </Button>
+            <Button ghost width="40%" type="success" onClick={() => setStep(3)}>
+              Accept
+            </Button>
+          </div>
         </div>
         <div
           className={`${
             step != 3 && 'hidden'
-          } flex items-center justify-center flex-col w-full h-[70vh]`}
+          } flex items-center justify-center flex-col gap-5 h-[70vh]`}
         >
-          <BadgeCheck size={120} />
-          <h2 className="tracking-widest text-6xl mt-5">{gpc(6)}</h2>
-          <p className="leading-4 text-gray-400 text-center">
-            Please provide this code to your driver.
-          </p>
-          <Button
-            onClick={() => navigate('/services')}
-            marginTop="2vh"
-            type="secondary"
-          >
+          <UserSearch size={120} />
+          <Input htmlType="number" placeholder="Enter Client Code" />
+          <Button width="40%" type="secondary" onClick={handleSubmit}>
             All set!
           </Button>
         </div>
