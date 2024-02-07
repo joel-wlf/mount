5b03a65b8d2ef9152181a9ac3517bd52e16622ce
Joel <72318130+joel-wlf@users.noreply.github.com>
2024-02-07 17:06:47 +0100

    update Car.jsx

--- src/pages/Car.jsx
+++ src/pages/Car.jsx
@@ -1,13 +1,16 @@
 import { useBodyScroll } from '@geist-ui/core';
-import { Radio } from 'lucide-react';
+import { ArrowRight, CarTaxiFront, Radio } from 'lucide-react';
 import EnergyChart from '../components/EnergyChart.jsx';
 import EnergyStorage from '../components/EnergyStorage.jsx';
 import FeatureCard from '../components/FeatureCard.jsx';
 import Navbar from '../components/Navbar';
 import batteryData from '../data/batteryData.js';
+import { useNavigate } from 'react-router-dom';
 
 function Car() {
   const [hidden, setHidden] = useBodyScroll();
+
+  const navigate = useNavigate();
 
   function toggleScroll(state) {
     setHidden(state);
@@ -37,6 +40,20 @@ function Car() {
           title="Wireless Charging"
           initial={true}
         />
+
+        <div
+          onClick={() => navigate('/services')}
+          className="flex items-center justify-start flex-row border-[1px] border-[#333] rounded-[10px] w-full my-3 p-5"
+        >
+          <div className="pr-5">
+            <CarTaxiFront size={45} />
+          </div>
+          <div className="mr-auto">
+            <h4 className="m-0 flex items-center gap-2">
+              Car Sharing <ArrowRight size={18} />
+            </h4>
+          </div>
+        </div>
       </div>
     </>
   );
